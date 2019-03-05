c119e093d1dbd56822b26e424db0081dab005192
Nane Kratzke <nane.kratzke@th-luebeck.de>
2019-03-05 07:35:54 +0000

    Update web/main.dart

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,5 @@
 import 'dart:html';
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is still running.';
+  querySelector('#output').text = 'Hooray, your first barebones Dart app is running!';
 }
