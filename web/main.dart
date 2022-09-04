c6696eb91dca45b2223a7b13384a1cb166018425
Paras Adhikari <paras.adhikari@stud.th-luebeck.de>
2022-09-04 16:38:12 +0200

    renamed ball to bird

--- web/main.dart
+++ web/main.dart
@@ -4,7 +4,7 @@ import 'dart:html';
 import 'dart:math';
 import 'dart:async';
 final field = querySelector("#field");
-final ball = querySelector("#field .ball");
+final bird = querySelector("#field .bird");
 final gameover = querySelector("#gameover");
 final sbutton = querySelector("#sbutton");
 var pipeUp = querySelector("#field .pipe_up");
@@ -14,18 +14,18 @@ final points = querySelector("#points");
 
 final fieldwidth = field?.getBoundingClientRect().width.toInt() ?? 0; //700
 final fieldheight = field?.getBoundingClientRect().height.toInt() ?? 0; //600
-final ballHeight = ball?.getBoundingClientRect().height.toInt() ?? 0; //50
+final birdHeight = bird?.getBoundingClientRect().height.toInt() ?? 0; //50
 
 class Field{
   int width;
   int height;
-  late Ball myBall;
+  late Bird myBird;
   late Pipe pipes;
-  Symbol running = #on;
+  Symbol gamestate = #on;
   int pipesPassed = 0;
   
   Field(this.width, this.height){
-    myBall = Ball(this, ballHeight);
+    myBird = Bird(this, birdHeight);
     pipes = Pipe(this);
   }
 }
@@ -44,20 +44,20 @@ class Pipe{
   
 }
 
-class Ball{
-  int ballHeight;
+class Bird{
+  int birdHeight;
   Field field;
   int distanceFromTop = 10;
   
-  Ball(this.field, this.ballHeight);
+  Bird(this.field, this.birdHeight);
   
-  void ballDownwards(){
-    distanceFromTop = distanceFromTop + 10;           //ball down
+  void birdDownwards(){
+    distanceFromTop = distanceFromTop + 10;           //bird down
     
     
   }
   
-  void ballUpwards(){ 
+  void birdUpwards(){ 
     distanceFromTop -= 50;
   }
 }
@@ -68,13 +68,13 @@ class View{
   
   void update() {
       
-      //UPDATE BALL POSITION & POINTS
-      ball?.style.top = '${newField.myBall.distanceFromTop}px';
+      //UPDATE BIRD POSITION & POINTS
+      bird?.style.top = '${newField.myBird.distanceFromTop}px';
       points?.text = 'Points: ${newField.pipesPassed}';
       
       //VALUES NEEDED FOR COLLISION
-      int ballTop = newField.myBall.distanceFromTop;
-      var ballBottom = ballTop + 35; // 35 ball height
+      int birdTop = newField.myBird.distanceFromTop;
+      var birdBottom = birdTop + 35; // 35 bird height
       var fieldLeft = field?.getBoundingClientRect().left.toInt() ?? 0;
       var pipeUpLeft = (pipeUp?.getBoundingClientRect().left.toInt() ?? 0) - fieldLeft;
       //subtract fieldLeft because it is different for different screens
@@ -85,15 +85,15 @@ class View{
        pipes are in the same x axis ↔️ of the bird 
        && if bird is in the same y axis ↕️ as pipes 
       */
-      bool collision = (ballTop < newField.pipes.pipeUpBottom && pipeUpLeft < 150 && pipeUpLeft > 50) ||
-                        (ballBottom > newField.pipes.pipeDownTop && pipeDownLeft < 150 && pipeDownLeft > 50);
+      bool collision = (birdTop < newField.pipes.pipeUpBottom && pipeUpLeft < 150 && pipeUpLeft > 50) ||
+                        (birdBottom > newField.pipes.pipeDownTop && pipeDownLeft < 150 && pipeDownLeft > 50);
     
       //print("${fieldLeft},${pipeUpLeft}");
     
       //IF BIRD TOUCHES GROUND || COLLISION
-      if(ballTop >= (fieldheight - ballHeight) || collision){
-        newField.running = #off;
-        ballTop -= - 2;                                 //freeze the ball
+      if(birdTop >= (fieldheight - birdHeight) || collision){
+        newField.gamestate = #off;
+        birdTop -= - 2;                                 //freeze the bird
         gameover?.style.display = "inline-block";       //show buttons
         sbutton?.style.display = "inline-block";
         pipeUp?.style.animation = "stopped";            //stop pipe animation
@@ -118,38 +118,38 @@ void main() {
   
   //GAME START
   sbutton?.onClick.listen((_) async {                               //if start clicked run the main loop
-      newField.running = #on;
+      newField.gamestate = #on;
       gameover?.style.display = "none";                             //hide gameover text
       sbutton?.style.display = "none";                              //hide button
       pipeUp?.style.display = "inline-block";                       //show pipes & points tally
       pipeDown?.style.display = "inline-block";
       points?.style.display = "inline-block";
-      newField.pipesPassed = 0;                                              //reset point count
-      newField.myBall.distanceFromTop = 0;                          //reset ball position
+      newField.pipesPassed = 0;                                     //reset point count
+      newField.myBird.distanceFromTop = 0;                          //reset bird position
       pipeUp?.style.animation = "pipeanim 3s infinite linear";      //resume pipe animation
       pipeDown?.style.animation = "pipeanim 3s infinite linear";
       
       //MAIN LOOP
       Timer.periodic(Duration(milliseconds: 50), (timer){
-        newField.myBall.ballDownwards();
+        newField.myBird.birdDownwards();
         view.update();
-        if(newField.running == #off) timer.cancel();
+        if(newField.gamestate == #off) timer.cancel();
       });
    });
     
    //KEY PRESS EVENTLISTENER 
   window.onKeyUp.listen((_) {
-    if(newField.running == #on){
-      newField.myBall.ballUpwards();
+    if(newField.gamestate == #on){
+      newField.myBird.birdUpwards();
       view.update();
     }
   });
   
   //NEW ANIMATION EVENTLISTENER
-  pipeUp?.on['animationiteration'].listen((_) {                    //runs before new animation
-    int pipeUpTop = Random().nextInt(400) * -1;                    //random pipe_up top from 0 to -400
+  pipeUp?.on['animationiteration'].listen((_) {                      //runs before new animation
+    int pipeUpTop = Random().nextInt(400) * -1;                      //random pipe_up top from 0 to -400
     pipeUp?.style.top = "${pipeUpTop}px";              
-    pipeDown?.style.top = "${pipeUpTop + 600}px";                  //pipe_down top is pipe_up top + pipe_up height + gap (200)
+    pipeDown?.style.top = "${pipeUpTop + 600}px";                    //pipe_down top is pipe_up top + pipe_up height + gap (200)
     
     //SAVE PIPE DIMENSIONS FOR COLLISION TEST
     int pipeUpBottom = pipeUpTop + 400;
